29a19ed072f9d5a2c90142601655843a095f7e27
Shanahan-Suresh <SHANAHAN.SURESH1@GMAIL.COM>
2022-03-04 15:34:14 +0800

    Initial upload
    Adds the latest backend neural network code and all available UI skeleton files.

---
+++ extract_data.py
@@ -0,0 +1,179 @@
+import pandas as pd
+import numpy as np
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+
+#Opens an excel sheet
+def open_sheet():
+    import openpyxl
+
+    #saves excel file as a 'workbook' object, wb
+    wb = openpyxl.load_workbook('Data.xlsx', data_only=True)
+
+    #retuns the contents of the first sheet in workbook
+    return wb[('Sheet1')]
+
+#Obtains a table's parameter values from the dataset
+def get_parameters(initial_row,initial_column):
+    properties = []
+    values = []
+
+    for rows in range (initial_row, initial_row+2):
+
+        for columns in range (initial_column+1, initial_column+4):
+            cell_value = sheet.cell(row=rows,column=columns).value
+
+            #cell values only saved as a 'property' or 'value' in their respective lists
+            if rows == initial_row :
+                properties.append(cell_value)
+
+            else :
+                values.append([cell_value])
+
+
+    #create dictionary from completed lists
+    parameters = dict(zip(properties, values))
+
+    global df
+    df = pd.DataFrame(parameters) #build dataframe from obtained parameters
+
+#Obtains a table's chemical names, properties, and values from the dataset
+def get_chemicals(initial_row,initial_column):
+
+    properties_tag = []
+    chemical_names = []
+    blank_row = False
+    rows = initial_row
+
+    #appends parameter labels into an array
+    for columns in range(initial_column,initial_column+4):
+
+        cell_value = sheet.cell(row=rows,column=columns).value
+
+        if(cell_value == None):
+
+            if sheet.cell(row=rows-2,column=columns-1).value == "Parameters":
+                cell_value="Composition, g"
+
+            elif sheet.cell(row=rows-2,column=columns-2).value == "Parameters":
+                cell_value="SR file number"
+
+            else:
+                cell_value="Chemical Temperature"
+
+        properties_tag.append(cell_value)
+
+    rows += 1
+
+    #appends acid names into an array
+    while (not blank_row):
+
+        for columns in range (initial_column,initial_column+4):
+            cell_value = sheet.cell(row=rows,column=columns).value
+            chemical_names.append([cell_value])
+
+        next_cell_value = sheet.cell(row=rows+1,column=columns).value
+
+        #exits loop if next cell is empty
+        if next_cell_value == None :
+            blank_row = True
+            
+        rows = rows +1
+
+        chemicals = dict(zip(properties_tag, chemical_names)) #combine the two arrays into a dictionary structure
+
+        #builds a temporary dataframe from obtained values
+        df1= pd.DataFrame(chemicals)
+        bigdata=pd.concat([df, df1], axis=1, join='inner')
+        global finaldf
+        finaldf = pd.concat([finaldf,bigdata],ignore_index=True) #concatanates the temporary dataframe into the final dataframe
+        del bigdata
+
+        chemical_names.clear() #clear chemical names for next iteration
+
+#Obtains a table's parameter values from the dataset
+def findParameterData():
+    global finaldf
+    finaldf = pd.DataFrame()
+
+    #hardcoded loop parameters based on dataset
+    initial_row = 2
+    initial_column = 1
+    max_row = sheet.max_row
+    max_col = sheet.max_column
+
+    #loops through every row first
+    while initial_row != max_row:
+        cell_value = sheet.cell(row=initial_row,column=initial_column).value
+
+        #ASSUMES that after finding the word 'parameters', there will be chemicals properties and so on
+        if cell_value == "Parameters":
+            get_parameters(initial_row, initial_column)  #Obtains parameter values
+            get_chemicals(initial_row+2, initial_column) #Obtains chemical values
+
+        initial_row += 1
+
+        #ASSUMES that a new table begins every 5 columns
+        if initial_row == max_row:
+            initial_column += 5
+
+            #exit loop at final coloumn, else row increment by 2
+            if initial_column >=  max_col:
+                break
+
+            else:
+                initial_row=2
+
+#Converts the obtained data into a modified dataframe
+def modifyDataset():
+    finaldf['Composition, g'].astype(str)
+    finaldf['Composition, g'] = finaldf['Composition, g'].fillna(0)
+    finaldf.loc[finaldf['Composition, g'].str.contains("Nil",na = False),'Composition, g'] = 0
+    finaldf['Aluminium Temperature'] = finaldf['Aluminium Temperature'].fillna(0)
+    finaldf['Chemical Temperature'] = finaldf['Chemical Temperature'].fillna(0)
+    finaldf['Chemicals'] = finaldf.Chemicals.astype(str)
+    finaldf.loc[finaldf['Chemicals'].str.contains("Lauric acid",na=False) & finaldf['Chemicals'].str.contains("Stearic acid",na=False), 'Chemicals'] = 4
+    finaldf.loc[finaldf['Chemicals'].str.contains("Control",na=False),'Chemicals'] = 0
+    finaldf.loc[finaldf['Chemicals'].str.contains("Lauric acid",na = False),'Chemicals'] = 1
+    finaldf.loc[finaldf['Chemicals'].str.contains("Stearic acid",na=False),'Chemicals'] = 2
+    finaldf.loc[finaldf['Chemicals'].str.contains("Parafin Wax",na=False),'Chemicals'] = 3
+
+    finaldf['Lauric Acid'] = np.where((finaldf['Chemicals'] == 1) | (finaldf['Chemicals'] == 4), 1, 0)
+    finaldf['Stearic Acid'] = np.where((finaldf['Chemicals'] == 2) | (finaldf['Chemicals'] == 4), 1, 0)
+    finaldf['Parafin Wax'] = np.where((finaldf['Chemicals'] == 3), 1, 0)
+    finaldf['Parafin Wax Composition'] = 0
+    finaldf['Lauric Acid Composition'] = 0
+    finaldf['Stearic Acid Composition'] = 0
+
+    for i in range(len(finaldf)):
+
+        if (finaldf["Lauric Acid"].iloc[i] == 1) & (finaldf["Stearic Acid"].iloc[i] == 1):
+
+            temparr = finaldf.loc[i, 'Composition, g']
+            split = temparr.split("\n")
+            finaldf.loc[i, 'Lauric Acid Composition'] = int(split[0])
+            finaldf.loc[i, 'Stearic Acid Composition'] = int(split[1])
+
+        elif (finaldf.loc[i, 'Lauric Acid'] == 1) & (finaldf.loc[i, 'Stearic Acid'] != 1):
+            finaldf.loc[i, 'Lauric Acid Composition'] = finaldf._get_value(i,'Composition, g')
+
+        elif (finaldf.loc[i, 'Stearic Acid'] == 1) & (finaldf["Lauric Acid"].iloc[i] != 1):
+            finaldf.loc[i, 'Stearic Acid Composition'] = finaldf._get_value(i,'Composition, g')
+
+        elif finaldf.loc[i, 'Parafin Wax'] == 1:
+            finaldf.loc[i, 'Parafin Wax Composition'] = finaldf._get_value(i,'Composition, g')
+
+    data = finaldf.drop(["Chemicals","Composition, g","SR file number"], axis=1)
+    return data
+
+#Main Function
+def main():
+
+    global sheet #sheet set as global value
+    sheet = open_sheet()
+    findParameterData()
+    data = modifyDataset()
+
+    return data, finaldf #returns to neural_network function
+
+main()
---
+++ test_network.py
@@ -0,0 +1,340 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from os.path import  exists
+from glob import glob
+from torchvision import transforms
+from PIL import Image
+import math
+import extract_data as extract
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
+
+#Normalise the dataset
+def create_dataset(data):
+    # normalize the data to be in range [0,1]
+    NormData = (data-data.min()) / (data.max() - data.min())
+    return NormData
+
+
+#Splits dataframe into subsets and returns the values
+def split_dataset(data):
+    rows = len(data.index)
+    split_percentage = math.floor(rows *(90/100)) #split amount
+
+    training_set = data.iloc[:split_percentage , :] #all rows before the split index
+    test_set = data.iloc[split_percentage: , :] #all rows after the split index
+
+    return (training_set, test_set)
+
+
+#Opens and accesses the data of the raw data files in order to convert them into tensor
+def getOriginalImage(file_num):
+
+
+    #hardcoded path name and file type
+    file_path1 = "Raw Data/1.0/SR000" + str(file_num) + ".BMT"
+    file_path2 = "Raw Data/1.0/SR00" + str(file_num) + ".BMT"
+    file_exist1 = exists(file_path1)
+    file_exist2 = exists(file_path2)
+    if file_exist1:
+        img = Image.open(file_path1)
+    elif file_exist2:
+        img = Image.open(file_path2)
+    else:
+        return [0,0,0]
+    convert_tensor = transforms.ToTensor()
+    OriginalImageTensor = convert_tensor(img)
+
+    if OriginalImageTensor.shape[1] != 120 or OriginalImageTensor.shape[2] !=160:
+        OriginalImageTensor = F.interpolate(OriginalImageTensor,160)
+        OriginalImageTensor = OriginalImageTensor.swapaxes(1,2)
+        OriginalImageTensor = F.interpolate(OriginalImageTensor,120)
+        OriginalImageTensor = OriginalImageTensor.swapaxes(1,2)
+
+
+
+
+
+    #print(OriginalImageTensor.shape)
+
+    OriginalImageTensor = OriginalImageTensor.swapaxes(0,1)
+    OriginalImageTensor = OriginalImageTensor.swapaxes(1,2)
+    return OriginalImageTensor
+
+
+#Trains the neural network to convert data into an image
+def training(training_data):
+
+    #set up loss functions
+    criterion = nn.CrossEntropyLoss()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = Net()
+    model = model.to(device) #CUDA CODE
+    class_instance = model.float()
+    optimizer = optim.SGD(class_instance.parameters(), lr=0.009, momentum=0.9)
+    loss_function = nn.L1Loss()
+    total_loss = 0
+    count = 0
+
+    #trains network for each image (non-epoch)
+    for epoch in range(500):
+
+        for i in range(len(training_data)):
+            tensor = torch.tensor(training_data.iloc[i].values)
+            tensor = tensor.to(device) #CUDA CODE
+            OriginalImageTensor = getOriginalImage(finaldf['SR file number'].iloc[i])#tensor of the original image
+
+            #skip if empty image
+            if OriginalImageTensor == [0,0,0]:
+                continue
+
+            #get the dimensions of the original image
+            #OriginalImageheight = OriginalImageTensor.shape[0]
+            #OriginalImagewidth = OriginalImageTensor.shape[1]
+            OriginalImageTensor = OriginalImageTensor.unsqueeze(0)
+            
+            OriginalImageTensor = OriginalImageTensor.to(device) #CUDA CODE
+
+            #produce tensor of neural network image using previous dimensions
+            ProducedImageTensor = class_instance(tensor.float(),120,160) #tensor of the produced image
+            ProducedImageTensor = ProducedImageTensor.to(device) #CUDA CODE
+
+            #loss functions
+            optimizer.zero_grad()
+            loss = loss_function(ProducedImageTensor, OriginalImageTensor)
+            total_loss += loss
+            loss.backward()
+            optimizer.step() #optimizer
+            
+            #print comparisons
+            if epoch == 499:
+                if count == 50 :
+                    OriginalImageTensor = OriginalImageTensor.cpu().detach().numpy()
+                    plt.imshow(OriginalImageTensor[0])
+                    plt.show()
+                    ProducedImageTensor = ProducedImageTensor.cpu().detach().numpy()
+                    plt.imshow(ProducedImageTensor[0])
+                    plt.show()
+                    count = 0
+                count += 1
+
+        print("Epoch number:" + str(epoch))
+        print("Current epoch loss : {}".format(loss))
+        print("Total loss : {}\n".format(total_loss/len(training_data)))
+        total_loss = 0
+
+    save_model(class_instance)
+    print("Training completed")
+
+#Saves the current neural network model
+def save_model(model):
+    torch.save(model.state_dict(),'500-network')
+
+#Loads a trained neural network model
+def load_model(data):
+    print(data)
+    model = Net()
+    model.load_state_dict(torch.load('savetesting'))
+    '''for var_name in model.state_dict():
+        print(var_name, "\t", model.state_dict()[var_name])
+    model.eval()'''
+    output = model(data.float(),120,160)
+    print(output.shape)
+
+#Neural Network
+class Net(nn.Module):
+
+    #Below are the layers applied during training
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(11, 2048)
+        self.batchnorm1 = nn.BatchNorm2d(64, track_running_stats=False)
+        self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.25)
+        self.batchnorm2 = nn.BatchNorm2d(128, track_running_stats=False)
+
+        self.dropout2 = nn.Dropout2d(0.25)
+
+        self.fc2 = nn.Linear(2048,9600)
+
+        self.Convolution1 = nn.Conv2d(36,64, (3, 3), padding=1)
+        self.dropout3 = nn.Dropout2d(0.25)
+        self.batchnorm3 = nn.BatchNorm2d(64, track_running_stats=False)
+        self.Convolution2 = nn.Conv2d(64, 128, (3, 3), padding=1)
+        self.Convolution3 = nn.Conv2d(128, 64, (3, 3), padding = 1)
+        self.Convolution4 = nn.Conv2d(64, 3, (3, 3), padding = 1)
+
+
+
+
+    # x represents our data
+    def forward(self, x, originalimageheight, originalimagewidth):
+
+
+        torch.set_printoptions(threshold=10_000)
+
+        out = self.fc1(x)
+        leakRelu = nn.LeakyReLU(0.1)
+        out = leakRelu(out)
+        #out = self.dropout1(out)
+        out = self.fc2(out)
+
+        test_shape = torch.reshape(out, (12,20,40)) #Formula : input_size = channels * sqr_root(dimension)
+
+
+        #changes the shape of the data
+        test_shape = F.interpolate(test_shape,originalimagewidth, mode='linear', align_corners = True)
+        test_shape = test_shape.permute(0,2,1)
+        test_shape = F.interpolate(test_shape,originalimageheight, mode='linear', align_corners = True)
+        test_shape = test_shape.permute(2,1,0)
+        test_shape = F.interpolate(test_shape,36)
+        test_shape = test_shape.permute(2,1,0)
+        test_shape = test_shape.permute(0,2,1)
+
+        test_shape = test_shape.unsqueeze(0)
+
+        #1st Convolutional Layer
+        test_shape=self.Convolution1(test_shape)
+        test_shape=self.batchnorm1(test_shape)
+        leakRelu = nn.LeakyReLU(0.1)
+        test_shape=leakRelu(test_shape)
+        #test_shape=self.dropout3(test_shape)
+
+        #2nd Convolutional Layer
+        test_shape=self.Convolution2(test_shape)
+        test_shape=self.batchnorm2(test_shape)
+        leakRelu = nn.LeakyReLU(0.1)
+        test_shape=leakRelu(test_shape)
+        #test_shape=self.dropout3(test_shape)
+
+
+        #Last Convolutional Layer
+        test_shape = self.Convolution3(test_shape)
+        test_shape = self.batchnorm3(test_shape)
+        leakRelu = nn.LeakyReLU(0.1)
+        test_shape = leakRelu(test_shape)
+
+        #last layer
+        test_shape = self.Convolution4(test_shape)
+        #test_shape=leakRelu(test_shape)
+        test_shape = test_shape.swapaxes(1,2) #reverse order to 'width, height, channels'
+        test_shape = test_shape.swapaxes(2,3)
+        return test_shape
+
+#Saves a single tensor as an image to file
+def save_image_from_tensor(image_tensor, image_size = 500):
+
+    #swap tensor axes so 'channels' is first
+    image_tensor = image_tensor.swapaxes(2,1)
+    image_tensor = image_tensor.swapaxes(1,0)
+
+    #resize image
+    transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize(size=500),
+    transforms.ToTensor()
+    ])
+    image_tensor = transform(image_tensor)
+
+    #save image
+    save_image(image_tensor, 'test.png')
+
+#Evaluates the neural network based on test dataset
+def performance_evaluation(test_data, training_data_count):
+    original_images = []
+    predicted_images = []
+
+    for i in range(len(test_data)):
+        
+        OriginalImageTensor = getOriginalImage(finaldf['SR file number'].iloc[training_data_count + i])#tensor of the original test image
+        if OriginalImageTensor == [0,0,0]:
+                continue
+
+        OriginalImageTensor = OriginalImageTensor.unsqueeze(0)
+        OriginalImageTensor = OriginalImageTensor.detach().numpy()
+        original_images.append(OriginalImageTensor)
+
+        test_tensor = torch.tensor(test_data.iloc[i].values)
+        predicted_images.append(load_model(test_tensor))
+
+    #mean squared error formula
+    MSE = np.square(np.subtract(original_images,predicted_images)).mean()
+
+    print('Mean Squared Error : {}'.format(MSE))
+
+def main():
+    global finaldf
+    data, finaldf = extract.main() #data and final dataframe(pandas format) obtained from extract_data function
+
+    normalized_data = create_dataset(data)
+    training_data, test_data = split_dataset(normalized_data)
+    training(training_data)
+
+   #load_model(torch.tensor(test_data.iloc[1].values))
+
+   #training_data_count = len(training_data)
+   #performance_evaluation(test_data, training_data_count)
+
+main()
+
+'''------------------ Unused Code Area ------------------
+
+        #Individual Shape Viewing Commands
+        
+        #ProducedImagenumpy = ProducedImageTensor.detach().numpy()
+        #OriginalImagenumpy = OriginalImageTensor.detach().numpy()
+        #print(OriginalImageTensor.shape)
+        #print(ProducedImageTensor.shape)
+        
+        #Display Image commands
+        #plt.imshow(test_shape[0])
+        #plt.show()
+
+
+#Returns a list of tensors from BMT images located in current directory (and sub-dirs)
+def get_original_image_tensors():
+    original_image_tensors = []
+
+    images_dir = "**"
+    images_list = glob(os.path.join(images_dir, "*.BMT"))
+
+
+    for img_filename in images_list:
+        img_path = os.path.join(images_dir, img_filename)
+        original_image_tensors.append(getOriginalImage(img_filename))
+
+    return original_image_tensors
+
+
+#Converts a single image into a tensor
+def getOriginalImage(image_name):
+    img = Image.open(image_name)
+    convert_tensor = transforms.ToTensor()
+    OriginalImageTensor = convert_tensor(img)
+    OriginalImageTensor = OriginalImageTensor.permute(1, 2, 0)
+
+    return OriginalImageTensor
+
+#CUDA CODE
+# setting device on GPU if available, else CPU
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        print('Using device:', device)
+        print()
+
+#Additional Info when using cuda
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB\n\n')
+
+'''
