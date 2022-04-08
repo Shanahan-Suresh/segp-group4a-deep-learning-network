46b0c1870e1273050a855ab611bb13e24536d000
Shanahan-Suresh <SHANAHAN.SURESH1@GMAIL.COM>
2022-04-08 13:17:00 +0800

    Update network parameters to 19.2k version
    Details of the network are as follows:
    - Output nodes extended to 19200 (matches image dimension)
    - Leaky_ReLU operations have been changed to ReLU operations
    - Removed batch normalization from layers
    - Removed MaxPool2d layer

--- test_neural_network.py
+++ test_neural_network.py
@@ -59,9 +59,10 @@ def save_image_from_tensor(image_tensor, height=500, width=666):
 
 class Net(nn.Module):
 
+    #Below are the layers applied during training
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(11, 2048)
+        self.fc1 = nn.Linear(11, 2000)
         self.batchnorm1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU()
         self.dropout1 = nn.Dropout(0.25)
@@ -69,61 +70,70 @@ class Net(nn.Module):
 
         self.dropout2 = nn.Dropout2d(0.25)
 
-        self.fc2 = nn.Linear(2048, 9600)
+        self.fc2 = nn.Linear(2000,19200)
 
-        self.Convolution1 = nn.Conv2d(36, 64, (3, 3), padding=1)
+        self.Convolution1 = nn.Conv2d(36,64, (3, 3), padding=1, bias = True)
         self.dropout3 = nn.Dropout2d(0.25)
         self.batchnorm3 = nn.BatchNorm2d(64)
-        self.Convolution2 = nn.Conv2d(64, 128, (3, 3), padding=1)
-        self.Convolution3 = nn.Conv2d(128, 64, (3, 3), padding=1)
-        self.Convolution4 = nn.Conv2d(64, 3, (3, 3), padding=1)
+        self.Convolution2 = nn.Conv2d(64, 128, (3, 3), padding=1, bias = True)
+        self.Convolution3 = nn.Conv2d(128, 64, (3, 3), padding = 1, bias = True)
+        self.Convolution4 = nn.Conv2d(64, 3, (3, 3), padding = 1, bias = True)
+
+
+
 
     # x represents our data
     def forward(self, x, originalimageheight, originalimagewidth):
+
+
         torch.set_printoptions(threshold=10_000)
 
         out = self.fc1(x)
-        leakRelu = nn.LeakyReLU(0.1)
+        leakRelu = nn.ReLU()
         out = leakRelu(out)
-        # out = self.dropout1(out)
+        #out = self.dropout1(out)
         out = self.fc2(out)
 
-        test_shape = torch.reshape(out, (12, 20, 40))  # Formula : input_size = channels * sqr_root(dimension)
+        test_shape = torch.reshape(out, (12,40,40)) #Formula : input_size = channels * sqr_root(dimension)
 
-        # changes the shape of the data
-        test_shape = F.interpolate(test_shape, originalimagewidth, mode='linear', align_corners=True)
-        test_shape = test_shape.permute(0, 2, 1)
-        test_shape = F.interpolate(test_shape, originalimageheight, mode='linear', align_corners=True)
-        test_shape = test_shape.permute(2, 1, 0)
-        test_shape = F.interpolate(test_shape, 36)
-        test_shape = test_shape.permute(2, 1, 0)
-        test_shape = test_shape.permute(0, 2, 1)
+
+        #changes the shape of the data
+        test_shape = F.interpolate(test_shape,originalimagewidth, mode='linear', align_corners = True)
+        test_shape = test_shape.permute(0,2,1)
+        test_shape = F.interpolate(test_shape,originalimageheight, mode='linear', align_corners = True)
+        test_shape = test_shape.permute(2,1,0)
+        test_shape = F.interpolate(test_shape,36)
+        test_shape = test_shape.permute(2,1,0)
+        test_shape = test_shape.permute(0,2,1)
 
         test_shape = test_shape.unsqueeze(0)
 
-        # 1st Convolutional Layer
-        test_shape = self.Convolution1(test_shape)
-        test_shape = self.batchnorm1(test_shape)
-        leakRelu = nn.LeakyReLU(0.1)
-        test_shape = leakRelu(test_shape)
-        # test_shape=self.dropout3(test_shape)
+        #1st Convolutional Layer
+        test_shape=self.Convolution1(test_shape)
+        #test_shape=self.batchnorm1(test_shape)
+        leakRelu = nn.ReLU()
+        test_shape=leakRelu(test_shape)
+        #test_shape=self.dropout3(test_shape)
 
-        # 2nd Convolutional Layer
-        test_shape = self.Convolution2(test_shape)
-        test_shape = self.batchnorm2(test_shape)
-        leakRelu = nn.LeakyReLU(0.1)
-        test_shape = leakRelu(test_shape)
-        # test_shape=self.dropout3(test_shape)
+        #2nd Convolutional Layer
+        test_shape=self.Convolution2(test_shape)
+       # test_shape=self.batchnorm2(test_shape)
+        leakRelu = nn.ReLU()
+        test_shape=leakRelu(test_shape)
+        #test_shape=self.dropout3(test_shape)
 
-        # Last Convolutional Layer
+
+        #Last Convolutional Layer
         test_shape = self.Convolution3(test_shape)
-        test_shape = self.batchnorm3(test_shape)
-        leakRelu = nn.LeakyReLU(0.1)
+        #test_shape = self.batchnorm3(test_shape)
+        leakRelu = nn.ReLU()
         test_shape = leakRelu(test_shape)
 
-        # last layer
+        #last layer
         test_shape = self.Convolution4(test_shape)
-        # test_shape=leakRelu(test_shape)
-        test_shape = test_shape.swapaxes(1, 2)  # reverse order to 'width, height, channels'
-        test_shape = test_shape.swapaxes(2, 3)
+        #m = nn.MaxPool2d(3, stride=1, padding=1)
+        #test_shape = m(test_shape)
+        #test_shape=leakRelu(test_shape)
+        test_shape = test_shape.swapaxes(1,2) #reverse order to 'width, height, channels'
+        test_shape = test_shape.swapaxes(2,3)
         return test_shape
