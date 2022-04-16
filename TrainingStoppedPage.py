f1a6f24c7501217af2582c42617c0b1695bb7bb5
äbubakar20-02 <abubakarx72@gmail.com>
2022-04-16 09:37:19 +0800

    training stopped pop up made

---
+++ TrainingStoppedPage.py
@@ -0,0 +1,60 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QObject
+from PyQt5.QtGui import QIcon
+
+import CSS
+
+
+class Ui_MainWindow(QObject):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setFixedSize(261, 77)
+        MainWindow.setWindowFlag(QtCore.Qt.WindowCloseButtonHint, False)
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.MessageText = QtWidgets.QLabel(self.centralwidget)
+        self.MessageText.setGeometry(QtCore.QRect(70, 20, 161, 16))
+        self.MessageText.setObjectName("MessageText")
+
+        self.closeButton = QtWidgets.QPushButton(self.centralwidget)
+        self.closeButton.setGeometry(QtCore.QRect(180, 50, 75, 23))
+        self.closeButton.setObjectName("closeButton")
+
+        self.icon = QtWidgets.QLabel(self.centralwidget)
+        self.icon.setGeometry(QtCore.QRect(10, 10, 51, 41))
+        self.icon.setStyleSheet("background-color: rgb(255, 0, 0);")
+        self.icon.setText("")
+        self.icon.setObjectName("icon")
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Stop training"))
+        self.closeButton.setText(_translate("MainWindow", "Close"))
+
+class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setStyleSheet(CSS.BackgroundCSS)
+        self.setWindowIcon(QIcon('TrainingIcon.png'))
+
+        self.icon.setStyleSheet(CSS.CautionIconCSS)
+        self.MessageText.setText("Training has been stopped.")
+
+        self.closeButton.setStyleSheet(CSS.QPushButtonCSS)
+        self.closeButton.clicked.connect(self.close)
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = MyWindow()
+    MainWindow.show()
+    sys.exit(app.exec_())
